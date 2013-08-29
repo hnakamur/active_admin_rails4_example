c3b8ed3293f38b093c1b5b0fd5f19e79f8d424f4
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-29 15:13:45 +0900

    Add files generated by creating an app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActiveAdminRails4Example::Application.config.secret_key_base = '73def6e6900bf6107bdf97b00274ee5a66ec3ce6c559b543f6e27c8d1d1ce55aef9e21b09043d650d892e01b905184cebf72260990a2202959bcb0049c44a671'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ActiveAdminRails4Example::Application.config.session_store :cookie_store, key: '_active_admin_rails4_example_session'
