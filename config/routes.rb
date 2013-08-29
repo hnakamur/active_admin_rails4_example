bfad6d03896b0a32b7ba5ce000cdbe477e319109
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-29 15:45:10 +0900

    Add generated files by ActiveAdmin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActiveAdminRails4Example::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
